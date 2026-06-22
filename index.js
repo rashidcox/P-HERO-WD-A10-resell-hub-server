0278bc1f70204c932ec5859aa3fcf2dd75943eee
A rashid <coxringcox@gmail.com>
2026-06-23 03:28:45 +0600

    add data

--- index.js
+++ index.js
@@ -24,37 +24,59 @@ client
   })
   .catch();
 
+app.get(
+  "/api/users",
+  (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    console.log("Authorization header:", authHeader);
+    next();
+  },
+  async (req, res) => {
+    const users = await client
+      .db("auth_next_db")
+      .collection("user")
+      .find()
+      .toArray();
+    res.json(users);
+  },
+);
 
-// API endpoint to fetch all products
-app.get("/api/products", async (req, res) => {
-  try {
-    const productsCollection = client.db("resellHubDB").collection("products");
-    const products = await productsCollection.find().toArray();
-    res.json(products);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    res.status(500).json({ error: "Failed to fetch products" });
-  }
+// Add a new user
+app.post("/api/job", async (req, res) => {
+  const job = req.body;
+  const result = await await client
+    .db("auth_next_db")
+    .collection("Jobs")
+    .insertOne(job);
+  res.send(result);
 });
 
-// API endpoint to add a new product
-app.post("/api/products", async (req, res) => {
-  try {
-    const productsCollection = client.db("resellHubDB").collection("products");
-    const product = req.body;
-    const result = await productsCollection.insertOne(product);
-    res.json(result);
-  } catch (error) {
-    console.error("Error adding product:", error);
-    res.status(500).json({ error: "Failed to add product" });
-  }
+// Get jobs
+app.get("/api/jobs", async (req, res) => {
+  const jobs = await client
+    .db("auth_next_db")
+    .collection("Jobs")
+    .find()
+    .toArray();
+  res.json(jobs);
 });
 
+// add company
+app.post("/api/company", async (req, res) => {
+  const company = req.body;
+  const result = await await client
+    .db("auth_next_db")
+    .collection("company")
+    .insertOne(company);
+  res.send(result);
+});
 
+app.get("/", (req, res) => {
+  res.send("You can access the server APIII");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
 
 module.exports = app;
